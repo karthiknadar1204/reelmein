19641c078dd27a56ce714b266c2024ba6116d828
karthik <karthiknadar1204@gmail.com>
2025-04-30 18:14:17 +0530

    basic db setup

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./configs/schema.js",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL,
+    }
+  };
+  
+  
